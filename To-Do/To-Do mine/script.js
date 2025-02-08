f297e5e18933719d7c575b1e919790021922b09b
DavoDev-Hub <juanpajmz13@gmail.com>
2025-02-07 19:40:07 -0600

    My first Todo App

---
+++ To-Do/To-Do mine/script.js
@@ -0,0 +1,29 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click", addTodo);
+//todoList.addEventListener("click", deleteCheck);
+
+function addTodo(event) {
+  event.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo-div");
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i>✅</i>";
+  completedButton.classList.add("completed-button");
+  todoDiv.appendChild(completedButton);
+
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = "<i>🗑️</i>";
+  trashButton.classList.add("trash-button");
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+}
