ddb35fcb2ff851fdcb3d4a360d95dd3f08b879b2
DavoDev-Hub <juanpajmz13@gmail.com>
2025-03-03 17:21:22 -0600

    project finished

---
+++ courses/ModernJs2025/ConvertingDatatypes/script.js
@@ -0,0 +1,8 @@
+const userInput = document.querySelector("input");
+const send = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+send.addEventListener("click", function () {
+  ul.document.cc("li");
+  let current = userInput.value;
+});
