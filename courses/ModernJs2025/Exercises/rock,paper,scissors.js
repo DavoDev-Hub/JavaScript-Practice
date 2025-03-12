07f2c3b5acf5e5fc24c7106096a2018824d3a420
DavoDev-Hub <juanpajmz13@gmail.com>
2025-03-11 19:21:38 -0600

    training

---
+++ courses/ModernJs2025/Exercises/rock,paper,scissors.js
@@ -0,0 +1,20 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+// const start = function startGame() {
+//   console.log("Game is starting...");
+// };
+
+// method example
+// a mehtod is a function that is a property of an object
+// const person = {
+//     name: "John",
+//     greet: function () {
+//     console.log(`Hello ${this.name}`);
+//   },
+// };
+
+// person.greet(); // Hello John
+
+startGameBtn.addEventListener("click", function () {
+  console.log("Game is starting...");
+});
