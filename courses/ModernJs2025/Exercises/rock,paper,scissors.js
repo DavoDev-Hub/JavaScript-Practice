b22c6ca3f6720cdd7e762b41d42635dca2585940
DavoDev-Hub <juanpajmz13@gmail.com>
2025-03-11 21:06:50 -0600

    practice

--- courses/ModernJs2025/Exercises/rock,paper,scissors.js
+++ courses/ModernJs2025/Exercises/rock,paper,scissors.js
@@ -1,5 +1,31 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+const default_user_choice = "rock";
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
+  if (selection !== rock && selection !== paper && selection !== scissors) {
+    alert("Invalid choice! We chose Rock for you!");
+    default_user_choice = rock;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
+
 // const start = function startGame() {
 //   console.log("Game is starting...");
 // };
@@ -14,7 +40,3 @@ const startGameBtn = document.getElementById("start-game-btn");
 // };
 
 // person.greet(); // Hello John
-
-startGameBtn.addEventListener("click", function () {
-  console.log("Game is starting...");
-});
