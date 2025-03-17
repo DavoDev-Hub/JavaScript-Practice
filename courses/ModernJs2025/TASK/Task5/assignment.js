cd5506204cab2eb3f02aa9a03b63fed0e658180c
DavoDev-Hub <juanpajmz13@gmail.com>
2025-03-17 14:45:23 -0600

    practice

---
+++ courses/ModernJs2025/TASK/Task5/assignment.js
@@ -0,0 +1,16 @@
+// Task 1
+const task1_1 = document.querySelector("#task-1");
+const task1_2 = document.getElementById("task-1");
+const task1_3 = document.getElementsByTagName("li")[0];
+
+task1_1.style.backgroundColor = "black";
+task1_2.style.color = "white";
+
+// Task 2
+const task2_1 = document.querySelector("title");
+task2_1.textContent = "Assignment - Solved!";
+// document.title = "Assignment - Solved!"; // alternative
+
+// Task 3
+const h1 = document.getElementsByTagName("h1")[0];
+h1.textContent = "Assignment - Solved!";
