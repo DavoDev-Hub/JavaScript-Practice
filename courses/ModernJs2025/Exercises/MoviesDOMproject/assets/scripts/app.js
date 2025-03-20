7383d51255e7d349002fc7afa0b4c7becdd6ab41
DavoDev-Hub <juanpajmz13@gmail.com>
2025-03-19 21:34:27 -0600

    deleting

--- courses/ModernJs2025/Exercises/MoviesDOMproject/assets/scripts/app.js
+++ courses/ModernJs2025/Exercises/MoviesDOMproject/assets/scripts/app.js
@@ -1,25 +1,63 @@
-// const addMovieModal = document.getElementById("add-modal");
-// const addMovieModal = document.querySelector("#add-modal");
-const addMovieModal = document.body.children[1];
+const addMovieModal = document.getElementById("add-modal");
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector("header button");
-// const startAddMovieButton = document.querySelector("header").lastElementChild
-// const startAddMovieButton = document.querySelector("header").children[1];
-// const startAddMovieButton = document.querySelector("header").lastElementChild;
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
 const backdrop = document.getElementById("backdrop");
 // const backdrop = document.body.firstElementChild;
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
-
 const userInputs = addMovieModal.querySelectorAll("input");
-//const userInputs = addMovieModal.getElementsByNameTag("input");
+// const userInputs = addMovieModal.getElementsByTagName('input');
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+  // listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
 };
 
-const toggleMNovieModal = () => {
+const toggleMovieModal = () => {
+  // function() {}
   addMovieModal.classList.toggle("visible");
   toggleBackdrop();
 };
@@ -31,7 +69,8 @@ const clearMovieInput = () => {
 };
 
 const cancelAddMovieHandler = () => {
-  toggleMNovieModal();
+  toggleMovieModal();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
@@ -51,6 +90,7 @@ const addMovieHandler = () => {
   }
 
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue,
@@ -58,15 +98,22 @@ const addMovieHandler = () => {
 
   movies.push(newMovie);
   console.log(movies);
-  toggleMNovieModal();
+  toggleMovieModal();
   clearMovieInput();
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
+  updateUI();
 };
 
 const backdropClickHandler = () => {
-  toggleMNovieModal();
+  toggleMovieModal();
 };
 
-startAddMovieButton.addEventListener("click", toggleMNovieModal);
-backdrop.addEventListener("click", toggleMNovieModal);
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
 cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
 confirmAddMovieButton.addEventListener("click", addMovieHandler);
