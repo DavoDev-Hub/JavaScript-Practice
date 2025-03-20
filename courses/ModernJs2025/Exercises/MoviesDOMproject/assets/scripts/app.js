4a6a01d4c03d33694136500dd8ab179a01441b62
DavoDev-Hub <juanpajmz13@gmail.com>
2025-03-19 20:44:54 -0600

    validating user input

--- courses/ModernJs2025/Exercises/MoviesDOMproject/assets/scripts/app.js
+++ courses/ModernJs2025/Exercises/MoviesDOMproject/assets/scripts/app.js
@@ -8,6 +8,12 @@ const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
 // const backdrop = document.body.firstElementChild;
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+
+const userInputs = addMovieModal.querySelectorAll("input");
+//const userInputs = addMovieModal.getElementsByNameTag("input");
+
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -18,8 +24,31 @@ const toggleMNovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMNovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
 };
 
 const backdropClickHandler = () => {
@@ -28,4 +57,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener("click", toggleMNovieModal);
 backdrop.addEventListener("click", toggleMNovieModal);
-cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
